eee51167105dc0643eda056426b549dc74ce0320
Fenix Rhiulta <fenix.rhiulta@gmail.com>
2022-03-30 20:05:51 -0300

    Update

--- src/Scheduler.API/Program.cs
+++ src/Scheduler.API/Program.cs
@@ -1,9 +1,14 @@
+using Scheduler.API.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IRabbitMQService, RabbitMQService>();
 
 var app = builder.Build();
 
